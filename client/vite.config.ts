96ea81a084b6c960877eae5db0efb4454f350918
DewaSRY <sdewa6645@gmail.com>
2024-09-09 09:45:03 +0800

    init: setup workspace

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-
+import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: "../public", // make the budler to be express public file
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
